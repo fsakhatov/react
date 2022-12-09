66b7cd42383f932b492f23bca9595f24dda56bd5
farrukh26 <98838148+farrukhbek26@users.noreply.github.com>
2022-12-09 15:26:53 +0900

    Yana yangiii

---
+++ script.js
@@ -0,0 +1,11 @@
+// 12  What is a first order function
+//   13  What is a higher order function
+//   14  What is a unary function
+//   15  What is the currying function
+//   16  What is a pure function
+//   17  What is the purpose of the let keyword
+//   18  What is the difference between let and var
+//   19  What is the reason to choose the name let as a keyword
+//   20  How do you redeclare variables in switch block without an error
+//   21  What is the Temporal Dead Zone
+//   22  What is IIFE(Immediately Invoked Function Expression)
